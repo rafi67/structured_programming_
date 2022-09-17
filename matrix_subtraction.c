84897352ab0a2d41ecabce1c29c180aa16b4c7bc
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-18 00:11:23 +0600

    subtraction of matrix

---
+++ matrix_subtraction.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int matrix1[n][n], matrix2[n][n], matrix3[n][n];
+
+    for(int r=0; r<n; ++r) {
+        for(int c=0; c<n; ++c) scanf("%d", &matrix1[r][c]);
+    }
+
+    for(int r=0; r<n; ++r) {
+        for(int c=0; c<n; ++c) scanf("%d", &matrix2[r][c]);
+    }
+
+    for(int r=0; r<n; ++r) {
+        for(int c=0; c<n; ++c) {
+            matrix3[r][c] = matrix1[r][c]-matrix2[r][c];
+        }
+    }
+
+    for(int r=0; r<n; ++r) {
+        for(int c=0; c<n; ++c) printf("%d ", matrix3[r][c]);
+        printf("\n");
+    }
+
+    return 0;
+
+}
