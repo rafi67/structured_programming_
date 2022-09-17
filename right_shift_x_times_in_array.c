e66f3e473c53043b51fe3a4dcbbf5a363cfe950d
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-17 17:42:04 +0600

    right shift x times in array completed

--- right_shift_x_times_in_array.c
+++ right_shift_x_times_in_array.c
@@ -2,14 +2,19 @@
 
 int main() {
 
-    int n, x;
+    int n, x, n2;
     scanf("%d %d", &n, &x);
-
+    
     int a[n];
+    
+    n2 = n-x;
 
     for(int i=0; i<n; ++i) scanf("%d", &a[i]);
 
-    
+    for(int i=n2; i<n; ++i) printf("%d ", a[i]);
+
+    for(int i=0; i<n2; ++i) printf("%d ", a[i]);
+    printf("\n");
 
     return 0;
 
