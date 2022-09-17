f8e76321fab7e384ce1b0cf31f3d41c7c524684c
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-17 17:57:59 +0600

    maximum value in array

---
+++ maximum_value_in_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i=0; i<n; ++i) scanf("%d", &a[i]);
+
+    int m = a[0];
+
+    for(int i=0; i<n; ++i) {
+        if(a[i]>m) m = a[i];
+    }
+
+    printf("Maximum number in array is %d\n", m);
+
+    return 0;
+
+}
