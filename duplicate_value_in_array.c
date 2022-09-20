7155377c211f5b44b364f74da9c719ac50025c19
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-20 23:05:47 +0600

    duplicate data in array

--- duplicate_value_in_array.c
+++ duplicate_value_in_array.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
+void duplicate(int *a, int n) {
+    int a2[1000];
+    for(int i=0; i<1000; ++i) a2[i] = 0;
+
+    for(int i=0; i<n; ++i) {
+        for(int j=i+1; j<n; ++j) {
+            if(a[i]==a[j]) {
+                int index = a[i];
+                a2[index] = a[i];
+            }
+        }
+    }
+
+    for(int i=0; i<1000; ++i) {
+        if(a2[i]!=0) printf("%d ", a2[i]);
+    }
+    printf("\n");
+}
+
 int main() {
 
     int n, count = 0;
 
     scanf("%d", &n);
 
-    int a[n], a2[n];
+    int a[n];
 
     for(int i=0; i<n; ++i) scanf("%d", &a[i]);
 
-    for(int i=0; i<n; ++i) {
-        for(int j=i+1; j<n; ++j) {
-            if(a[i]==a[j]) {
-                a2[i] = 1; 
-                a2[j] = 1;
-            }
-        }
-    }
-
-    for(int i=0; i<n; ++i) if(a2[i]==1) ++count; 
-
-    printf("Number of duplicate value is %d\n", count);
+    duplicate(a, n);
 
     return 0;
 
