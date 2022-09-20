68f0cc52ff00cfa194be98c573754ee1c994162f
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-20 16:48:48 +0600

    division of matrix

---
+++ matrix_division.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int m1[n][n], m2[n][n];
+
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            scanf("%d", &m1[i][j]);
+        }
+    }
+    
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            scanf("%d", &m2[i][j]);
+        }
+    }
+    
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            printf("%d ", (m1[i][j]/m2[i][j]));
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
