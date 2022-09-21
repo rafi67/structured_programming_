7235680a2adeff3fe288268017c326ef7a03074b
Rafi Siddique <rafisiddique67@gmail.com>
2022-09-21 13:33:12 +0600

    sum of diagonal bracket added

--- sum_of_diagonal_matrix.c
+++ sum_of_diagonal_matrix.c
@@ -9,7 +9,9 @@ int main() {
     int a[n][n];
 
     for(int i=0; i<n; ++i) {
-        for(int j=0; j<n; ++j) scanf("%d", &a[i][j]);
+        for(int j=0; j<n; ++j) {
+            scanf("%d", &a[i][j]);
+        }
     }
 
     for(int i=0, j=n-1; i<n; ++i, --j) {
